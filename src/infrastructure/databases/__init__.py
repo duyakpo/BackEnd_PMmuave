5fa2990bfb2d844b48c465c58f7bc558894257a2
phamtuan77 <tuanpv8212@ut.edu.vn>
2025-08-15 22:14:53 +0700

    Update __init__.py

--- src/infrastructure/databases/__init__.py
+++ src/infrastructure/databases/__init__.py
@@ -1,5 +1,5 @@
 from infrastructure.databases.mssql import init_mssql
-from infrastructure.models import todo_model, user, event, survey, sponor, sponor_event, ticket, survey_response, ticket_type, invoice, invoice_line
+from infrastructure.models import todo_model, user_model, event_model, survey, sponsor_model, sponor_event, ticket, survey_response, ticket_type, invoice, invoice_line, checkin_model, sponsor_response
 
 def init_db(app):
     init_mssql(app)
