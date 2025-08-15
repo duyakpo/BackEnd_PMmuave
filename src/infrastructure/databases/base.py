c67ce0d1f912d1943046295725f82d3658dc161b
Nguyen Duy <duyakpo@gmail.com>
2025-08-15 23:33:16 +0700

    Add more class and relationship

--- src/infrastructure/databases/base.py
+++ src/infrastructure/databases/base.py
@@ -1,3 +1,11 @@
-from sqlalchemy.orm import declarative_base
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "mssql+pyodbc://sa:Aa123456@localhost:1433/pmmuave_db?driver=ODBC+Driver+17+for+SQL+Server"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- src/infrastructure/models/checkin_model.py
+++ src/infrastructure/models/checkin_model.py
@@ -5,5 +5,9 @@ class CheckinModel(Base):
     __tablename__ = "checkins"
     id = Column(Integer, primary_key=True)
     ticket_id = Column(Integer, ForeignKey("tickets.id"), nullable=False)
+    ticket = relationship("Ticket", back_populates="checkin")
+
     staff_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    staff = relationship("User", back_populates="checkins_made", foreign_keys=[staff_id])
+
     checkin_time = Column(DateTime, nullable=False)
