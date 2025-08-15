9f56f5a2279a36cb5ff85515d399a330d3491cff
phamtuan77 <tuanpv8212@ut.edu.vn>
2025-08-15 23:26:32 +0700

    Update survey.py

--- src/infrastructure/models/survey.py
+++ src/infrastructure/models/survey.py
@@ -1,11 +1,9 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from database import Base
-
-class Survey(Base):
-    __tablename__= "surveys"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    description = Column(String)
-    responses = relationship("SurveyResponse", back_populates="survey")
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
+from infrastructure.databases.base import Base
+class SurveyModel(Base):
+    __tablename__ = "surveys"
+    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+    title = Column(String(255), nullable=False)
+    sent_date = Column(DateTime, nullable=False)
     
