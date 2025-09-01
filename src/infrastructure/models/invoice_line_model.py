33747ea55d567287b8fdf0f237e1a3a172429948
Nguyen Duy <duyakpo@gmail.com>
2025-09-01 20:14:25 +0700

    Làm lại all model, bổ sung relationship

--- src/infrastructure/models/invoice_line_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
-from infrastructure.databases.base import Base
-class InvoiceLineModel(Base):
-    __tablename__ = "invoice_lines"
-    id = Column(Integer, primary_key=True)
-    invoice_id = Column(Integer, ForeignKey("invoices.id"), nullable=False)
-    ticket_id = Column(Integer, ForeignKey("tickets.id"), nullable=False)
-    quantity = Column(Integer, nullable=False)
-    amount = Column(Float, nullable=False)
