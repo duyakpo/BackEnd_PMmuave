66d14c97c71def74a8794ca0458d99ca6b37388f
HoangDinhPhung <phunghd7188@ut.edu.vn>
2025-09-04 13:35:21 +0700

    Remove BackEnd_PMmuave-main folder

--- BackEnd_PMmuave-main/src/api/ticketing_app/config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-class Config:
-    SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///ticketing.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- BackEnd_PMmuave-main/src/infrastructure/models/ticket_type_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
-from infrastructure.databases.base import Base
-class TicketTypeModel(Base):
-    __tablename__ = "ticket_types"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    price = Column(Float, nullable=False)
-    quantity = Column(Integer, nullable=False)
-    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
